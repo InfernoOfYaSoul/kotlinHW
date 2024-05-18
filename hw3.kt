1290e9d0a216521d8242361bdfd44cf6663751b0
CyberToad <78739353+InfernoOfYaSoul@users.noreply.github.com>
2024-05-18 16:00:31 +0300

    Add files via upload

---
+++ hw3.kt
@@ -0,0 +1,41 @@
+fun main(args: Array<String>) {
+    // Запрос размеров матриц и их элементов у пользователя
+    println("Введите размеры первой матрицы (число строк и столбцов):")
+    val (rows1, cols1) = readLine()!!.split(" ").map { it.toInt() }
+
+    println("Введите элементы первой матрицы:")
+    val matrix1 = Array(rows1) { DoubleArray(cols1) }
+    for (i in 0 until rows1) {
+        matrix1[i] = readLine()!!.split(" ").map { it.toDouble() }.toDoubleArray()
+    }
+
+    println("Введите размеры второй матрицы (число строк и столбцов):")
+    val (rows2, cols2) = readLine()!!.split(" ").map { it.toInt() }
+
+    println("Введите элементы второй матрицы:")
+    val matrix2 = Array(rows2) { DoubleArray(cols2) }
+    for (i in 0 until rows2) {
+        matrix2[i] = readLine()!!.split(" ").map { it.toDouble() }.toDoubleArray()
+    }
+
+    // Проверка входных данных
+    if (cols1 != cols2) {
+        println("Ошибка: количество столбцов в матрицах не совпадает.")
+        return
+    }
+    if (rows1 != rows2) {
+        println("Ошибка: количество строк в матрицах не совпадает.")
+        return
+    }
+
+    // Вычисление скалярного произведения
+    var scalarProduct = 0.0
+    for (i in 0 until rows1) {
+        for (j in 0 until cols1) {
+            scalarProduct += matrix1[i][j] * matrix2[i][j]
+        }
+    }
+
+    // Вывод результата
+    println("Скалярное произведение: $scalarProduct")
+}
